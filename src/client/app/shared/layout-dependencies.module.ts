308d498d7618b7c5dafd4c2016e631c00cc9902c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-06-12 04:28:53 +0200

    navbar styling

--- src/client/app/shared/layout-dependencies.module.ts
+++ src/client/app/shared/layout-dependencies.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MdToolbarModule, MdIconModule } from '@angular/material';
+import { MdToolbarModule, MdIconModule, MdButtonModule } from '@angular/material';
 
 /**
  * All layout related external modules to simplify module imports.
  */
 @NgModule({
-  imports: [FlexLayoutModule, MdToolbarModule, MdIconModule],
-  exports: [FlexLayoutModule, MdToolbarModule, MdIconModule]
+  imports: [FlexLayoutModule, MdToolbarModule, MdIconModule, MdButtonModule],
+  exports: [FlexLayoutModule, MdToolbarModule, MdIconModule, MdButtonModule]
 })
 export class LayoutDependenciesModule { }
