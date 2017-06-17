e4965eef1d1a2e638e252fc5aad727397490a698
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-06-17 13:52:11 +0200

    card layout to material deps

--- src/client/app/shared/layout-dependencies.module.ts
+++ src/client/app/shared/layout-dependencies.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MdToolbarModule, MdIconModule, MdButtonModule, MdTabsModule } from '@angular/material';
+import { 
+  MdToolbarModule, MdIconModule, MdButtonModule, MdTabsModule, MdCardModule 
+} from '@angular/material';
 
 /**
  * All layout related external modules to simplify module imports.
  */
 @NgModule({
-  imports: [FlexLayoutModule, MdToolbarModule, MdIconModule, MdButtonModule, MdTabsModule],
-  exports: [FlexLayoutModule, MdToolbarModule, MdIconModule, MdButtonModule, MdTabsModule]
+  imports: [FlexLayoutModule, MdToolbarModule, MdIconModule, 
+    MdButtonModule, MdTabsModule, MdCardModule],
+  exports: [FlexLayoutModule, MdToolbarModule, MdIconModule, 
+    MdButtonModule, MdTabsModule, MdCardModule]
 })
 export class LayoutDependenciesModule { }
